591b9ed5019aa651f564b1dece5e880b8f8f5c00
Gowtham1392 <48799978+Gowtham1392@users.noreply.github.com>
2019-04-01 20:18:50 +0530

    Add files via upload

---
+++ getInput.py
@@ -0,0 +1,4 @@
+firstName = input("Enter first name: ")
+lastName = input("Enter last name: ")
+age = input("Enter your Age: ")
+print("Your Name is "+firstName+" "+lastName+" and Your Age is "+str(age))
